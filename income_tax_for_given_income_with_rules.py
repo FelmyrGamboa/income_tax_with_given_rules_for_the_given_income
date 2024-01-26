58d5c625922bc987f99197600cba201e3cb344a2
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-26 13:13:20 +0800

    Pseudocodes

--- income_tax_for_given_income_with_rules.py
+++ income_tax_for_given_income_with_rules.py
@@ -5,3 +5,9 @@
 # For example, suppose the taxable income is 45000, and the income tax payable is
 
 # 10000*0% + 10000*10%  + 25000*20% = $6000.
+
+#Create a variable to store the value of the given taxable income
+
+#Create three conditions that will evaluate the taxable income to get the income tax payable base on the given conditions
+
+#Display the outcome of the computing the payable income tax
