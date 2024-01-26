097712585574661c22474a8d611f1443dc152a4a
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-26 13:35:17 +0800

    Displayed the outcome

--- income_tax_for_given_income_with_rules.py
+++ income_tax_for_given_income_with_rules.py
@@ -11,12 +11,13 @@ income = 45000
 
 #Create three conditions that will evaluate the taxable income to get the income tax payable base on the given conditions
 if income <= 10000:
-    print("You are excempted in paying tax")
+    payable_tax = 10000 * 0
 
 elif income <= 20000:
     payable_tax = 10000 * 0.1
 
 else: 
-    payable_tax = (10000 * 0.1) + (income - 2000) * 0.2
+    payable_tax = (10000 * 0.1) + (income - 20000) * 0.2
 
 #Display the outcome of the computing the payable income tax
+print(f"The taxable income is {income} and the income tax payable is {round(payable_tax)}.")
