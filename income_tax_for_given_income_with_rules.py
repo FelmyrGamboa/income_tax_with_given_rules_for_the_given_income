cac02369090504fe20dd3709accbcc7bd74fe084
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-26 13:23:45 +0800

    Completed creating the three conditions that will evaluate and get the taxable amount

--- income_tax_for_given_income_with_rules.py
+++ income_tax_for_given_income_with_rules.py
@@ -7,7 +7,16 @@
 # 10000*0% + 10000*10%  + 25000*20% = $6000.
 
 #Create a variable to store the value of the given taxable income
+income = 45000
 
 #Create three conditions that will evaluate the taxable income to get the income tax payable base on the given conditions
+if income <= 10000:
+    print("You are excempted in paying tax")
+
+elif income <= 20000:
+    payable_tax = 10000 * 0.1
+
+else: 
+    payable_tax = (10000 * 0.1) + (income - 2000) * 0.2
 
 #Display the outcome of the computing the payable income tax
